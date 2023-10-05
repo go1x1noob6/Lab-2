389acabe0c1db4b94e533e60bb1cdfc35b1cfc33
user <user@DESKTOP-BFFI4GQ>
2023-10-05 12:17:23 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int len1, len2;
+	cin >> len1; // Ввод кол-во в миллиметров
+	cin >> len2; // Ввод кол-во в сантиметров
+
+	len2 += len1 / 10;
+	cout << len2 << endl;
+
+	return 0
+
+}
